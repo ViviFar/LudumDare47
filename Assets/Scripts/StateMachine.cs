b2480b7ec4d3c9673422c894c778467114846ad1
ViviFar <vivien.f@live.fr>
2020-10-02 22:36:17 +0200

    first commit: setting up project

---
+++ Assets/Scripts/StateMachine.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour
+{
+    #region singleton
+    private static StateMachine instance;
+    public static StateMachine Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<StateMachine>();
+                if (instance == null)
+                {
+                    GameObject go = new GameObject();
+                    instance = go.AddComponent<StateMachine>();
+                    go.name = "StateMachine";
+                }
+            }
+            return instance;
+        }
+    }
+    #endregion
+
+    public GameStates currentState = GameStates.Menu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
