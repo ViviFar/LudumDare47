fa0026706d7df4a463f3a415baf6978c6f8a5bc7
ViviFar <vivien.f@live.fr>
2020-10-03 12:52:30 +0200

    add pause system in game to go back to menu, quit or resume game

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject PausePanel;
+
+    private bool paused = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PausePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!paused)
+            {
+                paused = true;
+                Time.timeScale = 0;
+                PausePanel.SetActive(true);
+            }
+            else
+            {
+                resume();
+            }
+        }
+    }
+
+    public void resume()
+    {
+        paused = false;
+        Time.timeScale = 1;
+        PausePanel.SetActive(false);
+    }
+
+    public void LoadMenu()
+    {
+        StateMachine.Instance.currentState = GameStates.Menu;
+    }
+
+    public void Close()
+    {
+        Application.Quit();
+    }
+}
--- Assets/Scripts/StateMachine.cs
+++ Assets/Scripts/StateMachine.cs
@@ -70,4 +70,6 @@ public class StateMachine : MonoBehaviour
     {
         currentState = newState;
     }
+
+
 }
