b32bc7cb6714ecd3694167783e4fd8bc93216bbe
ViviFar <vivien.f@live.fr>
2020-10-03 12:53:14 +0200

    add first character behaviour: move right and left and shoot

---
+++ Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    public float speed = 4.0f;
+
+    private void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.y += speed * Time.deltaTime;
+        transform.position = pos;
+        Destroy(gameObject, 5);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform bulletParent;
+    public float speed = 3.0f;
+
+    public float DelayBetweenShoots = 2.0f;
+
+    private bool canShoot = true;
+    private float timeSinceShoot = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //arbitrary decision: if the player enters both right and left arrows, right one will prevail
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector3 pos = transform.position;
+            pos.x += speed * Time.deltaTime;
+            transform.position = pos;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector3 pos = transform.position;
+            pos.x -= speed * Time.deltaTime;
+            transform.position = pos;
+        }
+
+        timeSinceShoot += Time.deltaTime;
+
+        //reset the ability to shoot if cooldown is over
+        if (timeSinceShoot > DelayBetweenShoots)
+        {
+            canShoot = true;
+        }
+
+        //shoot and lock the ability for the cooldown time
+        if (Input.GetKey(KeyCode.Space) && canShoot)
+        {
+            GameObject go = Instantiate(bulletPrefab, transform);
+            go.transform.SetParent(bulletParent);
+            canShoot = false;
+            timeSinceShoot = 0;
+        }
+    }
+}
