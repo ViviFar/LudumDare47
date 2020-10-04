b8f9ae8f03ab6ad7669f66c8d196185087785992
ViviFar <vivien.f@live.fr>
2020-10-04 18:23:11 +0200

    add main music theme to game

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : GenericSingleton<SoundManager>
+{
+    public AudioSource MainSource;
+    
+    public AudioClip loopedClip;
+    
+
+    protected override void Awake()
+    {
+        base.Awake();
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        MainSource.clip = loopedClip;
+        MainSource.loop = true;
+        MainSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
